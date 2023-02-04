6137b3a638ead3c95f8dbc928387a17859d3ae78
Natalia Ivanova <nataliaivanova@MacBook-Air-Natalia-2.local>
2023-02-04 19:36:18 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,13 @@
+TOKEN = '6164021795:AAG36I5Bnq3JZzQWXgRHKh8kCUmfkfZLGRk'
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'рубль': 'RUR',
+    'евро': 'EUR'
+}
+
+
+TIMEZONE = 'Moscow'
+TIMEZONE_COMMON_NAME = 'Moscow'
